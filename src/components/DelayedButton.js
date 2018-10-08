cde89d1a5b7abd7684a0d123b2348f2d53746585
Monica Tamaru Jones <mntamaru@gmail.com>
2018-10-08 19:22:19 +0000

    automatically backed up by learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 
 export default class DelayedButton extends Component {
 
-  timeOut(e, delay) {
+  setTimeout(e, delay) {
     this.props.delay = delay;
     
   }  
